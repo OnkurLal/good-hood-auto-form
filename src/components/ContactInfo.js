6305b9bdfac2c24941d95fdac81e80f07bdee95d
Onkur Lal <onkur24lal@gmail.com>
2023-04-18 07:27:59 -0400

    initial commit

---
+++ src/components/ContactInfo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ContactInfo(props) {
+  return (
+    <>
+      <div>
+        <h1>Contact Info</h1>
+      </div>
+    </>
+  );
+}
+
+export default ContactInfo;
---
+++ src/components/VehicleSelector.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function VehicleSelector(props) {
+  return (
+    <>
+      <div>
+        <h1>Vehcile Selection</h1>
+      </div>
+    </>
+  );
+}
+
+export default VehicleSelector;
